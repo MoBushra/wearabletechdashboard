9b42771dd057d9a10d7f9c5e4b4a8270c237a2d2
Mohamed Bushra <mohamed.bushra@ceracare.co.uk>
2022-06-21 14:58:11 +0100

    Complete topbar, comment out for now

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./topbar.css";
-import HomeIcon from '@material-ui/icons';
+import HomeIcon from '@mui/icons-material/Home';
+import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
+import HotelIcon from '@mui/icons-material/Hotel';
+import MoodIcon from '@mui/icons-material/Mood';
+import MedicationIcon from '@mui/icons-material/Medication';
+import WarningIcon from '@mui/icons-material/Warning';
 
 export default function TopBar() {
     return (
@@ -9,11 +14,32 @@ export default function TopBar() {
                 <div className="topLeft">
                     <span className="logo">Health Dashboard</span>
                 </div>
-                <div className="topRight">
-                    <div className="topbarIconContainer">
-                        <HomeIcon/>
-                    </div>
-                </div>
+                {/*<div className="topRight">*/}
+                {/*    <div className="topbarIconContainer">*/}
+                {/*        <HomeIcon/>*/}
+                {/*        <span className="topIconBadge">2</span>*/}
+                {/*    </div>*/}
+                {/*    <div className="topbarIconContainer">*/}
+                {/*        <MonitorHeartIcon/>*/}
+                {/*        <span className="topIconBadge">2</span>*/}
+                {/*    </div>*/}
+                {/*    <div className="topbarIconContainer">*/}
+                {/*        <HotelIcon/>*/}
+                {/*        <span className="topIconBadge">2</span>*/}
+                {/*    </div>*/}
+                {/*    <div className="topbarIconContainer">*/}
+                {/*        <MoodIcon/>*/}
+                {/*        <span className="topIconBadge">2</span>*/}
+                {/*    </div>*/}
+                {/*    <div className="topbarIconContainer">*/}
+                {/*        <MedicationIcon/>*/}
+                {/*        <span className="topIconBadge">2</span>*/}
+                {/*    </div>*/}
+                {/*    <div className="topbarIconContainer">*/}
+                {/*        <WarningIcon/>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
+
             </div>
         </div>
     )
