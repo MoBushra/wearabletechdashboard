19e15c12ddcc3a342b51b2da93d2d0b7e6006282
Mohamed Bushra <mohamed.bushra@ceracare.co.uk>
2022-06-22 09:59:30 +0100

    Add some dummy data to test graphs out

---
+++ src/data/dummyData.js
@@ -0,0 +1,30 @@
+export const userData = [
+    {
+        name: 'Page A',
+        "Active User": 4000,
+    },
+    {
+        name: 'Page B',
+        "Active User": 3000,
+    },
+    {
+        name: 'Page C',
+        "Active User": 2000,
+    },
+    {
+        name: 'Page D',
+        "Active User": 2780,
+    },
+    {
+        name: 'Page E',
+        "Active User": 1890,
+    },
+    {
+        name: 'Page F',
+        "Active User": 2390,
+    },
+    {
+        name: 'Page G',
+        "Active User": 3490,
+    },
+];
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,13 @@
 import "./home.css";
 import FeaturedInfo from "../../components/featuredInfo/featuredInfo";
+import Chart from "../../components/chart/chart";
+import { userData } from "../../data/dummyData";
 
 export default function Home() {
     return (
         <div className="home">
             <FeaturedInfo/>
+            <Chart data={userData} title="Heart Rate" grid dataKey="Active User"/>
         </div>
     )
 }
